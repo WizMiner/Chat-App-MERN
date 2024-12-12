135ef3a3abc8841123a11391bddd2e364e955a7d
Minte <mintesinottamene0917@gmail.com>
2024-12-12 10:21:21 +0300

    auth initial & MD connected also env added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
-import express from "express"; // Correct import syntax
-const app = express();
+import dotenv from "dotenv";
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
-const PORT = 5000;
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
