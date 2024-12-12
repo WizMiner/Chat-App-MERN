bdc7e1a6d974415e038c24866c7cbe244ddc2f37
Minte <mintesinottamene0917@gmail.com>
2024-12-12 14:54:49 +0300

    daisyUI config & test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,27 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="carousel carousel-vertical rounded-box h-100">
+      <div className="carousel-item h-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" />
+      </div>
+      <div className="carousel-item h-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" />
+      </div>
+      <div className="carousel-item h-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" />
+      </div>
+      <div className="carousel-item h-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" />
+      </div>
+      <div className="carousel-item h-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" />
+      </div>
+      <div className="carousel-item h-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" />
+      </div>
+      <div className="carousel-item h-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" />
+      </div>
+    </div>
+  );
 }
