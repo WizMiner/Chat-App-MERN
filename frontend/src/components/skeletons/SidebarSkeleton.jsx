9d0affd28ab6c890d026f5f05ee1956fe8ea1906
Minte <mintesinottamene0917@gmail.com>
2024-12-18 10:40:13 +0300

    Implement getReceiverSocketId to retrieve socketId by userId

---
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -0,0 +1,52 @@
+import { Users } from "lucide-react";
+
+/**
+ * The SidebarSkeleton component renders a list of skeleton elements
+ * to serve as placeholders while loading the contact list.
+ * The component is designed to be used as a loading state
+ * for the Sidebar component.
+ *
+ * @returns {JSX.Element} The skeleton contact list.
+ */
+const SidebarSkeleton = () => {
+  // Create 8 skeleton items
+  const skeletonContacts = Array(8).fill(null);
+
+  return (
+    <aside
+      className="h-full w-20 lg:w-72 border-r border-base-300 
+    flex flex-col transition-all duration-200"
+    >
+      {/* Header */}
+      <div className="border-b border-base-300 w-full p-5">
+        <div className="flex items-center gap-2">
+          <Users className="w-6 h-6" />
+          <span className="font-medium hidden lg:block">Contacts</span>
+        </div>
+      </div>
+
+      {/* Skeleton Contacts */}
+      <div className="overflow-y-auto w-full py-3">
+        {skeletonContacts.map((_, idx) => (
+          <div key={idx} className="w-full p-3 flex items-center gap-3">
+            {/* Avatar skeleton */}
+            <div className="relative mx-auto lg:mx-0">
+              {/* A skeleton avatar */}
+              <div className="skeleton size-12 rounded-full" />
+            </div>
+
+            {/* User info skeleton - only visible on larger screens */}
+            <div className="hidden lg:block text-left min-w-0 flex-1">
+              {/* A skeleton user name */}
+              <div className="skeleton h-4 w-32 mb-2" />
+              {/* A skeleton user online status */}
+              <div className="skeleton h-3 w-16" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </aside>
+  );
+};
+
+export default SidebarSkeleton;
